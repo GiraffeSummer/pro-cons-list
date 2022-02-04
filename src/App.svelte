a7d84dd38240d116a00abef6c80f4286840f5e8c
GiraffeSummer <gidoselten@hotmail.com>
2022-02-04 16:35:54 +0100

    added analitics to track site visits

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,14 @@
 <script>
   import Table from './Table.svelte';
   import { lists, createList, createListItem } from './store.js';
-
+  import Analytics from './Analytics.svelte';
 
   const addList = () => {
     $lists = [...$lists, createList('new list', [])];
   };
 </script>
 
+<Analytics />
 <div class="row">
   {#each $lists as list}
     <Table {list} />
